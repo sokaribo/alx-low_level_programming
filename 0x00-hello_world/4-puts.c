0b17ce2f0fa1fbd3e0992ab6b38b52d337b55a96
sokaribo <cotterellsoks@gmail.com>
2022-12-08 07:11:43 -0800

    4.Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- * main - print a line of code using puts
+ * main - print a line of code using putis
  *
  * Return: 0
 */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
