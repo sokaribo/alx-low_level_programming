2e71cf40cc6c20c4670b2fc55b3a002029149ebe
sokaribo <cotterellsoks@gmail.com>
2022-12-13 13:23:04 -0800

    10xalphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char letter;
 
-	while (count++ <= 9)
+	for (count = 0; count <= 9; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
