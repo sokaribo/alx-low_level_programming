2bcb73d2abf075d817af4808c45b776ced3f378a
sokaribo <cotterellsoks@gmail.com>
2022-12-15 04:16:10 -0800

    1.Like

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,13 @@ int i;
 printf("Infinite loop incomng :(\n");
 
 i = 0;
-
-/* while (i < 10)*/
-/* {*/
-/* putchar(i);*/
-/* }*/
-
-printf("Infinite loop avoided! \\o\/\n");
+/*
+ *while (i < 10
+ *{
+ *putchar(i);
+ *}
+ */
+printf("Infinite loop avoided \\o\/\n");
 
 return (0);
 }
