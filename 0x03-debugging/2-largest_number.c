850fb8272ecd9bb9c96fb0586d14b507d0e678dc
sokaribo <cotterellsoks@gmail.com>
2022-12-16 02:19:27 -0800

    2.0>972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,28 +8,25 @@
  * Return: largest number
  */
 
-int largest_number(int a; int b; int c)
+int largest_number(int a, int b, int c)
 {
 	int largest;
 
 	if (a > b)
 	{
-	if (a > c)
-	{
-	largest = a;
+		if (b > c)
+			largest = a;
+		else if (a > c)
+			largest = a;
+		else
+			largest = c;
 	}
 	else
 	{
-	largest = c;
-	}
-	}
-	else if (b > c)
-	{
-	largest = b;
-	}
-	else
-	{
-	largest = c;
+		if (b < c)
+			largest = c;
+		else
+			largest = b;
 	}
 
 	return (largest);
