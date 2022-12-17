78fa6e6ca321b48e26e80aff236418ddd9228b04
sokaribo <cotterellsoks@gmail.com>
2022-12-16 21:01:53 -0800

    11.The problem

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	unsigned long int i, n = 612852475143;
 
-	for (i = 3; i < 782849; i = i + 2)
+	for (i = 2; i < n; i++)
 	{
-		while ((n % i == 0) && (n != i)
+		while (n % i == o)
 				n = n / i;
 	}
 	printf("%lu\n", n);
