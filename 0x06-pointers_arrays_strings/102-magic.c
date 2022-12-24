e571b1779090271e5f85410448745ad8f3ed91ca
sokaribo <cotterellsoks@gmail.com>
2022-12-23 17:15:32 -0800

    10.A dream

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - prints
- * Return: 0
- */
 int main(void)
 {
 	int n;
@@ -12,13 +8,13 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/**
-	 * write your line code here
+	/*
+	 * write your line of code here...
 	 * Remember
-	 * _ you are not allowed to use a
-	 * _ you are not allowed to modify p
-	 * _ only one statement
-	 * _ you are not allowed to code anything else than this line of code
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n*/
