d0d8d7a9303fc1456f165b440688c648721613be
sokaribo <cotterellsoks@gmail.com>
2023-01-02 14:44:34 -0800

    1.memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memcpy - function copies @n bytes from memory area @src
+ * to memory area @dest
+ *
+ * @n: function copies
+ *
+ * @dest: source string
+ *
+ * @src: bytes from memory area
+ *
+ * Return: a pointer to @dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
