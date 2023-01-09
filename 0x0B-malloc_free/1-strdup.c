82ad0967b16ccb365fe020d247cae52870c60f4f
sokaribo <cotterellsoks@gmail.com>
2023-01-09 14:27:19 -0800

    1.The woman

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in
+ * memory which contains a copy of the string passed.
+ * @str: the source string
+ * Return: returns a pointer to the string duplicated. It returns NULL
+ * if insufficient memory was available.
+ */
+char *_strdup(char *str)
+{
+	char *nstr;
+	unsigned int len, i;
+
+	/* check if string is NULL */
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = 0;
+	while (str(len) != '\0')
+	{
+		len++;
+	}
+	nstr = malloc(sizeof(char) * (len + 1));
+	/* check if malloc was successful */
+	if (nstr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		nstr[i] = str[i];
+	}
+	nstr[len] = '\0';
+	return (nstr);
+}
