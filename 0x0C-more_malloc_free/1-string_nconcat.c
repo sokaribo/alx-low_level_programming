c4e79d636b36553ac333b5c0f21c90ea83a446c1
sokaribo <cotterellsoks@gmail.com>
2023-01-13 05:19:59 -0800

    1.string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	if (num >= _strlen(s2)) /* account for n too big */
 		num = _strlen(s2);
-	len = _strlen(s1) + 1; /* +1 to account for null pointer */
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
 	if (ptr == NULL)
 		return (NULL);
