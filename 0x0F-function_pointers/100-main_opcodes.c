56b368c4a9589fddb67592bf204469eb17fb1f29
sokaribo <cotterellsoks@gmail.com>
2023-01-19 07:05:55 -0800

    4.Most

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -26,7 +26,7 @@ int main(int argc, char argv[])
 	}
 	for (count = 0; count < bytes; count++)
 	{
-		printf("%02hhx", *((char *)main + count));
+		printf("%.2hhx", *((char *)main + count));
 		if (count < bytes - 1)
 			printf(" ");
 		else
