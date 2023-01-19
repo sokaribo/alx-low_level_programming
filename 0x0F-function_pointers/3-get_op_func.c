5e6d9851a1671dd45828afdf2dc38f25db3ddf8b
sokaribo <cotterellsoks@gmail.com>
2023-01-19 05:44:28 -0800

    3.A goal

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,14 +8,14 @@
  *
  * Return: correct function result or NULL if operator is wrong.
  */
-int (*get_op_func(char *s))(int int)
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+" op_add},
-		{"-" op_sub},
-		{"*" op_mul},
-		{"/" op_div},
-		{"%" op_mod},
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
 	};
 	int i;
