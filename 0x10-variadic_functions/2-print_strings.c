7d1963fc8e0cba812a49131317736ebef6a90a1a
sokaribo <cotterellsoks@gmail.com>
2023-01-25 15:31:12 -0800

    2.O woman's

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,29 +7,30 @@
  * print_strings - prints strings followed by a new line.
  * @separator: string to be printed between strings.
  * @n: number of strings.
- *
+ * @...: A variable of strings to be printed.
  * Return: void.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int;
-	char *s;
 	va_list strings;
+	char *str;
+	unsigned int index;
 
 	va_start(strings, n);
-	if (separator == NULL)
-		separator = "";
-	for (i = 0; i < n; i++)
+
+	for (index = 0; index < n; index++)
 	{
-		s = va_arg(strings, char *);
-		if (s == NULL)
-			s = "(nil)";
-		printf("%s", s);
-		if (i < n - 1)
-		{
+		str = va_arg(strings, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
-		}
 	}
 	printf("\n");
+
 	va_end(strings);
 }
